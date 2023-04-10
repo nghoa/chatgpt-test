085c5425b6b3bdcd2299bbfe0b8d87705038d5cd
Hoa Nguyen <hoa.nguyen@arineo.com>
2023-04-10 22:32:35 +0200

    chatengine test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Chat } from "@/components/chat";
 
 function App() {
 
   return (
     <div className="app">
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
